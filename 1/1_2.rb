2edbf32988333074157ada4e88db06012c0eb92d
iavivai <18yukitaka@gmail.com>
2019-11-24 20:51:41 +0900

    add test case 1_2

--- 1/1_2.rb
+++ 1/1_2.rb
@@ -1,4 +1,9 @@
-str = ''
+class First
+  def self.composition_str
+    str = ''
+    'パトカー'.each_char.with_index { |s, i| str += s + 'タクシー'[i] }
+    str
+  end
+end
 
-'パトカー'.each_char.with_index { |s, i| str += s + 'タクシー'[i] }
-puts str
+puts First.composition_str
--- test/1_test.rb
+++ test/1_test.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 require './1/1_0'
 require './1/1_1'
+require './1/1_2'
 
 class Test1 < Test::Unit::TestCase
   def test_1_0
@@ -9,5 +10,9 @@ class Test1 < Test::Unit::TestCase
 
   def test_1_1
     assert_equal 'タクシー', First.odd_index_str
+  end
+
+  def test_1_2
+    assert_equal 'パタトクカシーー', First.composition_str
   end
 end
